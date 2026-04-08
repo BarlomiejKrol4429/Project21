7b395cb1683ae93216d3c45c0eb3289037ab75e4
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2026-04-08 17:18:36 +0200

    game over + bugfixes to logic

--- app/src/main/java/com/example/project21/GameActivity.java
+++ app/src/main/java/com/example/project21/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.project21;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -22,12 +23,13 @@ public class GameActivity extends AppCompatActivity {
     private ArrayList<Integer> allCards;
     private int roundNumber = 1;
     private int dealersHiddenCard;
-    private int currentPlayerCard = 0;
-    private int currentDealerCard = 0;
-    private int dealerCardSum = 0;
-    private int visiblePlayerCardSum = 0;
-    private int playerCardSum = 0;
+    private int currentPlayerCard;
+    private int currentDealerCard;
+    private int dealerCardSum;
+    private int visiblePlayerCardSum;
+    private int playerCardSum;
     private boolean gameInputsEnabled = false;
+    private boolean loss = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class GameActivity extends AppCompatActivity {
         LinearLayout game_layout = findViewById(R.id.game);
         ImageButton standButton = findViewById(R.id.stand);
         ImageButton hitButton = findViewById(R.id.hit);
+        game_layout.setClickable(false);
 
         Drawable gbg = game_layout.getBackground();
         gbg.setFilterBitmap(false);
@@ -61,7 +64,16 @@ public class GameActivity extends AppCompatActivity {
         startGame();
     }
     public void startGame(){
-
+        d_card1.setImageResource(getResources().getIdentifier("card_q", "drawable", getPackageName()));
+        d_card1.getDrawable().setFilterBitmap(false);
+        Toast.makeText(GameActivity.this, roundNumber+"", Toast.LENGTH_SHORT).show();
+        currentPlayerCard = 0;
+        currentDealerCard = 0;
+        dealerCardSum = 0;
+        visiblePlayerCardSum = 0;
+        playerCardSum = 0;
+        allCards = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11));
+        checkAbilityToDraw();
         dealerCards = new ImageView[]{
                 d_card1, d_card2, d_card3, d_card4, d_card5, d_card6
         };
@@ -79,7 +91,7 @@ public class GameActivity extends AppCompatActivity {
             imageView.setVisibility(View.INVISIBLE);
         }
 
-        allCards = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11));
+
         dealersHiddenCard = drawDealerCard(allCards);
         d_card1.setVisibility(View.VISIBLE);
         currentDealerCard++;
@@ -123,49 +135,52 @@ public class GameActivity extends AppCompatActivity {
     }
     public void stand(View view) {
         if (gameInputsEnabled) {
-            while (dealerCardSum < 17 && visiblePlayerCardSum <= 21) {
+            if(dealerCardSum < 17 && visiblePlayerCardSum <= 21) {
                 int card = drawDealerCard(allCards);
-                if (currentDealerCard < dealerCards.length) {
-                    dealerCards[currentDealerCard].setImageResource(getResources().getIdentifier("card_" + card, "drawable", getPackageName()));
-                    dealerCards[currentDealerCard].getDrawable().setFilterBitmap(false);
-                    dealerCards[currentDealerCard].setVisibility(View.VISIBLE);
-                }
+                dealerCards[currentDealerCard].setImageResource(getResources().getIdentifier("card_" + card, "drawable", getPackageName()));
+                dealerCards[currentDealerCard].getDrawable().setFilterBitmap(false);
+                dealerCards[currentDealerCard].setVisibility(View.VISIBLE);
+                currentDealerCard++;
             }
         }
         gameInputsEnabled = false;
         roundEnd();
-        Toast.makeText(this, allCards+" "+visiblePlayerCardSum+" "+playerCardSum+" "+dealerCardSum, Toast.LENGTH_SHORT).show();
     }
     public void hit(View view) {
-        if (gameInputsEnabled && playerCardSum < 21) {
-            int card = drawPlayerCard(allCards);
-            playerCards[currentPlayerCard].setImageResource(
-                    getResources().getIdentifier("card_" + card, "drawable", getPackageName())
-            );
-            playerCards[currentPlayerCard].getDrawable().setFilterBitmap(false);
-            playerCards[currentPlayerCard].setVisibility(View.VISIBLE);
-            currentPlayerCard++;
-        }
-        if(dealerCardSum < 17 && visiblePlayerCardSum <= 21) {
-            int card = drawDealerCard(allCards);
-            if (currentDealerCard < dealerCards.length) {
+        if(gameInputsEnabled){
+            if (playerCardSum < 21) {
+                int card = drawPlayerCard(allCards);
+                playerCards[currentPlayerCard].setImageResource(
+                        getResources().getIdentifier("card_" + card, "drawable", getPackageName())
+                );
+                playerCards[currentPlayerCard].getDrawable().setFilterBitmap(false);
+                playerCards[currentPlayerCard].setVisibility(View.VISIBLE);
+                currentPlayerCard++;
+            }
+            if(dealerCardSum < 17 && visiblePlayerCardSum <= 21) {
+                int card = drawDealerCard(allCards);
                 dealerCards[currentDealerCard].setImageResource(getResources().getIdentifier("card_" + card, "drawable", getPackageName()));
                 dealerCards[currentDealerCard].getDrawable().setFilterBitmap(false);
                 dealerCards[currentDealerCard].setVisibility(View.VISIBLE);
+                currentDealerCard++;
             }
+            checkAbilityToDraw();
         }
-        checkAbilityToDraw();
     }
     public void checkAbilityToDraw(){
         ImageButton hitButton = findViewById(R.id.hit);
         if(playerCardSum > 21){
             hitButton.setBackground(ContextCompat.getDrawable(this, R.drawable.hit_blocked));
-        }else{
+        } else if (playerCardSum == 21) {
+            hitButton.setBackground(ContextCompat.getDrawable(this, R.drawable.hit_perfect));
+        }
+        else{
             hitButton.setBackground(ContextCompat.getDrawable(this, R.drawable.hit));
         }
     }
 
     public void roundEnd(){
+        Toast.makeText(GameActivity.this, playerCardSum+" "+dealerCardSum, Toast.LENGTH_SHORT).show();
         d_card1.setImageResource(getResources().getIdentifier("card_" + dealersHiddenCard, "drawable", getPackageName()));
         d_card1.getDrawable().setFilterBitmap(false);
         boolean playerBust = playerCardSum > 21;
@@ -179,16 +194,13 @@ public class GameActivity extends AppCompatActivity {
                 tie();
             }
         }
-
-        if (playerBust && !dealerBust) {
+        else if (playerBust && !dealerBust) {
             loss();
         }
-
-        if (!playerBust && dealerBust) {
+        else if (!playerBust && dealerBust) {
             win();
         }
-
-        if (playerCardSum > dealerCardSum) {
+        else if (playerCardSum > dealerCardSum) {
             win();
         } else if (dealerCardSum > playerCardSum) {
             loss();
@@ -197,15 +209,35 @@ public class GameActivity extends AppCompatActivity {
         }
     }
     public void win(){
+        LinearLayout game_layout = findViewById(R.id.game);
         if(roundNumber != 5){
             roundNumber += 1;
-            Toast.makeText(this, "you win this round!", Toast.LENGTH_SHORT).show();
+            game_layout.setClickable(true);
         }
     }
     public void loss(){
-        Toast.makeText(this, "you lose!", Toast.LENGTH_SHORT).show();
+        LinearLayout game_layout = findViewById(R.id.game);
+        game_layout.setClickable(true);
+        loss = true;
     }
     public void tie(){
-        Toast.makeText(this, "you've tied!", Toast.LENGTH_SHORT).show();
+        LinearLayout game_layout = findViewById(R.id.game);
+        game_layout.setClickable(true);
     }
+    public void continueGame(View view){
+        LinearLayout game_layout = findViewById(R.id.game);
+        if(game_layout.isClickable()){
+            if(loss){
+                Intent intent = new Intent(GameActivity.this, OverActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                gameInputsEnabled = true;
+                startGame();
+            }
+        }
+        game_layout.setClickable(false);
+        loss = false;
+    }
+
 }
--- app/src/main/java/com/example/project21/OverActivity.java
+++ app/src/main/java/com/example/project21/OverActivity.java
@@ -1,5 +1,34 @@
 package com.example.project21;
 
-public class OverActivity {
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OverActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actity_over);
+
+        LinearLayout game_layout = findViewById(R.id.over);
+        ImageView gameOver = findViewById(R.id.game_over);
+        ImageButton restartButton = findViewById(R.id.restart);
+
+        Drawable gbg = game_layout.getBackground();
+        gbg.setFilterBitmap(false);
+        gameOver.getDrawable().setFilterBitmap(false);
+        Drawable hitButtonBg = restartButton.getBackground();
+        hitButtonBg.setFilterBitmap(false);
+    }
+    public void play(View view) {
+        Intent intent = new Intent(OverActivity.this, GameActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
